15cf9edd9f4410bf0bd50a20831e5988dcfe80cc
Sam Selfridge <Sam.selfridge@gmail.com>
2019-04-26 15:31:57 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const entry = [
+  './client/index.js'
+];
+
+const output = {
+  path: path.resolve(__dirname, 'dist'),
+  publicPath: '/dist/',
+  filename: 'bundle.js',
+};
+
+module.exports = {
+  entry, output,
+  devtool: "eval-source-map",
+  module: {
+    loaders: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets:[ 'es2015', 'react', 'stage-2' ]
+        }
+      },
+      { 
+        test: /\.css$/, 
+        use: [ {loader: 'style-loader'} , {loader: 'css-loader'}], 
+        exclude: /(node_modules)/ 
+    }
+    ],
+  },
+};    
+          
