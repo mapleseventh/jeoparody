d30c9f75fbab217f5f717c45c012280de85a8d7e
Jason Kim <jasonkim@Jasons-MacBook-Pro.local>
2019-04-26 20:01:59 -0700

    Created server data, connected to database, init UserController

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const bodyParser = require('body-parser');
+const server = http.createServer(app);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/signup', (req, res) => {
+  
+  res.send("HEY YOU")
+})
+
+
+
+
+
+
+
+
+server.listen(8080, () => {
+  console.log('listening at http://localhost:8080');
+});
+
+module.exports = server;
